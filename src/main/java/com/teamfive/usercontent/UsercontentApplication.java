09b6c31544dc0cd691424b9a3f04496e60a54acd
jayjoshi <jay.joshi@coviam.com>
2019-01-19 13:44:25 +0530

    Initial commit

---
+++ src/main/java/com/teamfive/usercontent/UsercontentApplication.java
@@ -0,0 +1,14 @@
+package com.teamfive.usercontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercontentApplication.class, args);
+	}
+
+}
+
